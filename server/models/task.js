2ca23c6adb6c82402caa63cb82873db9f74d6be1
raxelf <frananda.rafael@gmail.com>
2026-05-20 13:35:49 +0700

    chore: setup express js

---
+++ server/models/task.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Task extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Task.init({
+    userId: DataTypes.INTEGER,
+    title: DataTypes.STRING,
+    description: DataTypes.STRING,
+    dueDate: DataTypes.DATE,
+    priority: DataTypes.ENUM('low', 'medium', 'high'),
+    isCompleted: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'Task',
+  });
+  return Task;
+};
