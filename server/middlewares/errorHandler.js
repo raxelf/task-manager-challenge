98046a7ef1cd2963e241c50e6939082de5d6c5d1
raxelf <frananda.rafael@gmail.com>
2026-05-20 17:38:50 +0700

    fix: error message variable

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -12,13 +12,13 @@ const errorHandler = (err, _req, res, _next) => {
     errorMessage = err.errors[0].message;
   } else if (err.message === "INVALID_CREDENTIALS") {
     statusCode = 401;
-    message = "Invalid email/password";
+    errorMessage = "Invalid email/password";
   } else if (
     err.message === "UNAUTHORIZED" ||
     err.name === "JsonWebTokenError"
   ) {
     statusCode = 401;
-    message = "Invalid token";
+    errorMessage = "Invalid token";
   }
 
   res.status(statusCode).json({
